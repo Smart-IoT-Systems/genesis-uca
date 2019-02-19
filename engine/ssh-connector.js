d9d476a500c25ac2837dec4539f7594a17b85b05
nicolasferry <nicolas.ferry@sintef.no>
2019-02-19 14:44:55 +0100

    Fix error with port number in sshclient

--- engine/ssh-connector.js
+++ engine/ssh-connector.js
@@ -1,32 +1,66 @@
 var Client = require('ssh2').Client;
 var fs = require('fs');
-var emitter = require('events').EventEmitter;
-var logger=require('./logger.js');
+var logger = require('./logger.js');
 
-var ssh_connector = function () {
+var ssh_connector = function (ip, port, username, key) {
 
-    var that = new emitter();
+    var that = {};
+    that.options={
+        host: ip,
+        port: parseInt(port),
+        username: username,
+        privateKey: fs.readFileSync(key)
+    };
 
-    that.executeCommand = function (ip, port, username, key, command) {
-        var conn = new Client();
-        conn.on('ready', function() {
-        logger.log('info','SSH Client :: ready');
-        conn.exec(command, function(err, stream) {
-            if (err) throw err;
-            stream.on('close', function(code, signal) {
-                logger.log('info','Stream :: close :: code: ' + code + ', signal: ' + signal);
-                conn.end();
-            }).on('data', function(data) {
-                logger.log('info','STDOUT: ' + data);
-            }).stderr.on('data', function(data) {
-                logger.log('error','STDERR: ' + data);
-            });
+    that.upload_file = function (file_path_src, file_path_tgt) {
+        return new Promise(function (resolve, reject) {
+            var conn = new Client();
+            conn.on('ready', function () {
+                conn.sftp(function (err, sftp) {
+                    if (err) {
+                        logger.log('error',err);
+                        reject(err);
+                        throw err;
+                    };
+                    var readStream = fs.createReadStream(file_path_src);
+                    var writeStream = sftp.createWriteStream(file_path_tgt);
+
+                    writeStream.on('close', function () {
+                        logger.log('info', "file transferred succesfully");
+                        resolve(file_path_tgt);
+                    });
+
+                    writeStream.on('end', function () {
+                        conn.close();
+                    });
+
+                    readStream.pipe(writeStream);
+                });
+            }).connect(that.options);
         });
-        }).connect({
-            host: ip,
-            port: port,
-            username: username,
-            privateKey: fs.readFileSync(key)
+    };
+
+    that.execute_command = function (command) {
+        return new Promise(function (resolve, reject) {
+            var conn = new Client();
+            conn.on('ready', function () {
+                conn.exec(command, function (err, stream) {
+                    if (err){
+                        logger.log('error',err);
+                        reject(err);
+                        throw err;
+                    };
+                    stream.on('close', function (code, signal) {
+                        logger.log('info', 'Stream :: close :: code: ' + code + ', signal: ' + signal);
+                        conn.end();
+                        resolve(signal);
+                    }).on('data', function (data) {
+                        logger.log('info', 'ssh: ' + data);
+                    }).stderr.on('data', function (data) {
+                        logger.log('error', 'ssh: ' + data);
+                    });
+                });
+            }).connect(that.options);
         });
     };
 
