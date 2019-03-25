f14980727fa1c0a435a808cd6aacb8ffe3bd5384
nicolasferry <nicolas.ferry@sintef.no>
2019-03-25 18:02:31 +0100

    arduino node

---
+++ repository/arduino.js
@@ -0,0 +1,18 @@
+var mm = require('../metamodel/allinone.js');
+var uuidv1 = require('uuid/v1');
+
+/******************************/
+/* Specific Arduino component */
+/******************************/
+var arduino = function (spec) {
+    var that = mm.software_node(spec); //the inheritance
+    that._type = "arduino";
+    that.name = spec.name || "Main";
+    that.nr_description = spec.nr_description || ""; 
+    that.sketch= spec.sketch || "/Users/ferrynico/Documents/Code/GeneSIS-gitlab/docker-node-red-thingml/doc/examples/LightSensorApp/LightSensorApp.thingml";
+    that.libraries = spec.libraries || ["Adafruit GFX Library","Adafruit ST7735 and ST7789 Library"];
+
+    return that;
+};
+
+module.exports = arduino;
