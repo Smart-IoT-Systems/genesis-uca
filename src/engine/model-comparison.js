94f7a13766efd2fc925eec474be60dc2f996332e
nicolasferry <nicolas.ferry@sintef.no>
2020-02-29 00:25:23 +0100

    fix problem with serial - first step

--- src/engine/model-comparison.js
+++ src/engine/model-comparison.js
@@ -100,15 +100,16 @@ var comparator = function (dm) {
                         if (target_links[i].isDeployer) {
                             result.list_of_added_links_deployer.push(target_links[i]);
                             //Need to update the other side of the link
-                            /*let other = dm.get_comp_name_from_port_id(target_links[i].src);
-                            console.log(">>>>>>"+JSON.stringify(target_links[i]));
+                            //In the long term should be handle by the resource in the link
+                            //i.e., stop and restar the other side or at least should be a property 
+                            let other = dm.get_comp_name_from_port_id(target_links[i].src);
                             if(dm.get_comp_name_from_port_id(target_links[i].src) === c.name){
                                 other = dm.get_comp_name_from_port_id(target_links[i].target);
                             }
                             let node_to_be_redeployed=dm.find_node_named(other);
                             let in_removed=false;
-                            for (var i in result.list_of_removed_components) {
-                                if(result.list_of_removed_components[i].name === node_to_be_redeployed.name){
+                            for (var k in result.list_of_removed_components) {
+                                if(result.list_of_removed_components[k].name === node_to_be_redeployed.name){
                                     in_removed=true;
                                 }
                             }
@@ -116,7 +117,7 @@ var comparator = function (dm) {
                                 result.list_of_removed_components.push(node_to_be_redeployed);
                                 result.list_of_added_hosted_components.push(node_to_be_redeployed);
                                 result.list_of_added_components.push(node_to_be_redeployed);
-                            }*/
+                            }
                         } else {
                             result.list_of_added_links.push(target_links[i]);
                         }
